d6a5dfffb849c169303903539f58ff008042d724
Yaya <cryayo@hotmail.com>
2019-02-12 13:05:46 +0100

    Finished
    all done

---
+++ script.js
@@ -0,0 +1,9 @@
+function showPopup (){
+	var popup = document.getElementById("popup");
+	popup.classList.add('show')
+}
+
+function hidePopup (){
+	var popu = document.getElementById("popup");
+	popup.classList.remove('show')
+}
